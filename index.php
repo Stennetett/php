0b91ce099bf2e76c5d298109ae4d6642784688c5
Stennetett <56960738+Stennetett@users.noreply.github.com>
2023-03-26 00:49:26 +0100

    Update index.php
    added mysql info

--- index.php
+++ index.php
@@ -2,8 +2,38 @@
 <html>
 <head>
 	<title>Simple PHP Website</title>
+	
 </head>
 <body>
+<?php
+
+$mysqli = new mysqli('localhost', 'debian-sys-maint', 'dmM1hsMuy60U8YtP', 'sys');
+
+
+//Kontrollerar teckentabell
+if (!$mysqli->set_charset("utf8")) {
+  echo "Fel vid inställning av teckentabell utf8: %s\n". $mysqli->error;
+}
+else {
+//   // Här kan du skriva ut något om du vill ha en bekräftelse på att det funkar
+echo "Nuvarande teckenkodningstabell: %s\n". $mysqli->character_set_name();
+}
+
+if ($mysqli->connect_errno) {
+  // Om något gått fel skrivs felmeddelande ut
+  echo "Misslyckades att ansluta till MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+?>
+	<?php
+
+
+// Här ska ni formulera en SQL-query som hämtar *alla fält och alla rader* från tabellen film.
+$sql = "SELECT * FROM coffie";
+
+// Här körs queryn mot databasen
+$result = $mysqli->query($sql);
+?>
 	<h1>Welcome to my website</h1>
 	<p>Today is <?php echo date("Y/m/d"); ?></p>
 	<form action="submit-form.php" method="post">
@@ -15,5 +45,12 @@
 		<br>
 		<input type="submit" value="Submit">
 	</form>
+	<?php
+
+
+while($myRow = $result->fetch_array()) {
+   echo "<tr><td>" . $myRow['id'] . "</td><td>" . $myRow['name'] . "</td></tr>\n";
+ }
+?>
 </body>
 </html>
