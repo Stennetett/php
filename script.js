2047d01f23c0e82602687a7d2ec3bdceb5682618
Stennetett <56960738+Stennetett@users.noreply.github.com>
2023-07-06 03:19:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ document.addEventListener('DOMContentLoaded', function () {
     // Call the function with the desired text
     setTextContent('Hello, this text is inserted dynamically!');
 });
+
+// Get references to the SVG and button elements
+const svg = document.getElementById('visual');
+const transformBtn = document.getElementById('transform-btn');
+
+// Add click event listener to the button
+transformBtn.addEventListener('click', function() {
+  // Modify the SVG properties or apply CSS styles
+  svg.style.transform = 'rotate(45deg)';
+  svg.style.fill = 'red';
+});
+In the above example, when the "Transform" button is clicked, the SVG is rotated by 45 degrees and the fill color is changed to red. You can modify the transformation or styles based on your specific requirements.
+
+Note: Make sure to place the JavaScript code either before the closing </body> tag or inside an event listener for the DOMContentLoaded event to ensure that the elements are available when the code is executed.
+
+Remember to link the JavaScript file to your HTML file using a <script> tag or include it directly within a <script> tag if you prefer.
+
+
+
+
+
+
