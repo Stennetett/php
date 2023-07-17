53de8fae2ba081317b01cc7c8306e1c83df7724c
Stennetett <56960738+Stennetett@users.noreply.github.com>
2023-07-17 19:33:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-$servername = "localhost";
-$username = "newuser";
-$password = "HxvRvyZsSA4h4RA@";
-// Create connection
-$conn = new mysqli($servername, $username, $password, "sys");
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT id, first_name, last_name FROM users";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  echo "<table><tr><th>ID</th><th>Name</th></tr>";
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "<tr><td>".$row["id"]."</td><td>".$row["first_name"]." ".$row["last_name"].>
-  }
-  echo "</table>";
-} else {
-  echo "0 results";
-}
-$conn->close();
+a
 ?>
