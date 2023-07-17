898a3c3a30ce8a3346b4715af3d7afcac2c60f36
Stennetett <56960738+Stennetett@users.noreply.github.com>
2023-07-17 19:20:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-    <?php
+<?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 $servername = "localhost";
@@ -11,26 +11,18 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$query = "SELECT * FROM day_plan";
-$result = $mysqli->query($query);
+$sql = "SELECT id, first_name, last_name FROM users";
+$result = $conn->query($sql);
 
-// Check if the query was successful
-if ($result) {
-    // Loop through the rows and display the data
-    while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row['id'] . "<br>";
-        echo "Time: " . $row['time'] . "<br>";
-        echo "Task: " . $row['task'] . "<br>";
-        echo "Description: " . $row['description'] . "<br><br>";
-    }
-
-    // Free the result set
-    $result->free();
+if ($result->num_rows > 0) {
+  echo "<table><tr><th>ID</th><th>Name</th></tr>";
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>".$row["id"]."</td><td>".$row["first_name"]." ".$row["last_name"].>
+  }
+  echo "</table>";
 } else {
-    echo "Error executing the query: " . $mysqli->error;
+  echo "0 results";
 }
-
-// Close the MySQLi connection
-$mysqli->close();
+$conn->close();
 ?>
-
