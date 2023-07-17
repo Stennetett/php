1e99fdec6552bbbfc6c7b76448bb4866e1954005
Stennetett <56960738+Stennetett@users.noreply.github.com>
2023-07-17 22:31:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,9 @@
-<!DOCTYPE html>
 <html>
 <head>
-  <title>My Basic HTML Page</title>
+<title>PHP Test</title>
 </head>
 <body>
-  <h1>Welcome to My Basic HTML Page</h1>
-
-  <p>This is a simple HTML page.</p>
-
-  <ul>
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-  </ul>
-
-  <p>Here's an image:</p>
-  <img src="image.jpg" alt="Sample Image">
-
-  <p>Visit <a href="https://www.example.com">Example Website</a> for more information.</p>
-
+<?php echo '<p>Hello World</p>'; ?>
 </body>
 </html>
 
