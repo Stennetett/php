3c3abaee8fee88861328be58bc6a3393b1ee1bf9
Stennetett <56960738+Stennetett@users.noreply.github.com>
2023-07-21 15:32:00 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="game">
+        <?php include 'game.html'; ?>
+    </div>
+
+    <div class="menu-row">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+
+</body>
+
+</html>
